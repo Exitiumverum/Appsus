5c2ad90b81a1e398eb517acac762795c79397b89
Dan Tayari <kisa@tayari.co.il>
2025-01-24 12:15:50 +0200

    Css work

---
+++ react-hooks-starter/apps/mail/cmps/Starred.jsx
@@ -0,0 +1,23 @@
+
+import { mailService } from "../services/mail.service.js"
+import { MailList } from "../cmps/MailList.jsx"
+
+export function MailStarred({ mails, isFilterOpen }) {
+
+    const { fullname, email } = mailService.loggedUser
+    // console.log(mails);
+
+    // console.log(isFilterOpen)
+    const INBOX_BTN = document.querySelector('.mail-filter-item.filter-inbox')
+
+
+    let filteredMails = mails.filter((mail) => mail.isStarred === true)
+    console.log('Inbox: ', filteredMails)
+    console.log('Inbox: ', email);
+
+    // console.log(INBOX_BTN);
+
+    return (
+        <MailList mails={filteredMails} isFilterOpen={isFilterOpen} />
+    )
+}
